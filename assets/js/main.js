8a961fd05775337bde2172f47871342c25829328
Vishesh Handa <me@vhanda.in>
2020-09-11 13:26:07 +0200

    Newsletter: Add success and fail views
    The form submission still doesn't work.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 var app = new Vue({
+  delimiters: ["<<", ">>"],
   el: "#app",
   data: {
     isOpen: false,
+    newsletter: {
+      fail: false,
+      failReason: "Pokemon are not alive",
+      success: false,
+    },
   },
 });
