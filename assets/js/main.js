54712af2414432f92608fb0dcf58a09ba90c2817
Vishesh Handa <me@vhanda.in>
2020-09-12 03:53:34 +0200

    Make the newsletter work
    Wow. This took so so much longer than expected.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,38 @@ var app = new Vue({
     },
   },
 });
+
+window.onload = function () {
+  function submitNewsletter() {
+    const XHR = new XMLHttpRequest();
+    const name = document.getElementById("gj-name").value;
+    const email = document.getElementById("gj-email").value;
+
+    XHR.addEventListener("load", function () {
+      if (XHR.status == 200) {
+        app.newsletter.success = true;
+      } else {
+        app.newsletter.fail = true;
+        app.newsletter.failReason = XHR.responseText;
+      }
+    });
+    XHR.addEventListener("error", function () {
+      app.newsletter.fail = true;
+      app.newsletter.failReason = "Unknown Error";
+    });
+
+    XHR.open(
+      "POST",
+      "https://us-central1-gitjournal-io.cloudfunctions.net/NewsletterAdd"
+    );
+    XHR.setRequestHeader("Content-Type", "application/json");
+    XHR.send(JSON.stringify({ name: name, email: email }));
+  }
+
+  const form = document.getElementById("gj-newsletter");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    submitNewsletter();
+  });
+};
